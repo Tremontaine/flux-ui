4b3cff2aa09b23dfc98b7de196f0712eb6ae5169
Tremontaine <76396298+Tremontaine@users.noreply.github.com>
2025-03-10 13:14:00 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const fetch = require('node-fetch');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3589;
 
 // Enable CORS for all routes
 app.use(cors());
